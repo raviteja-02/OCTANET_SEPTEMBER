20108d91ef0e987ec952c9721ec90d6b75318084
Raviteja <120414406+raviteja-02@users.noreply.github.com>
2023-09-17 12:23:04 +0530

    Added Completed button in Task 2

--- TASK 2 - TODO List/script.js
+++ TASK 2 - TODO List/script.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const todoList = document.querySelector(".js-todo-list");
     const todoInput = document.querySelector(".inputselect");
 
-    // Function to add a new todo
     function addTodo() {
         const todoText = todoInput.value.trim();
 
@@ -10,12 +9,22 @@ document.addEventListener("DOMContentLoaded", function () {
             const todoItem = document.createElement("li");
             todoItem.className = "todo-item";
 
-            // Create a span element for the todo text
             const todoTextElement = document.createElement("span");
             todoTextElement.textContent = todoText;
             todoTextElement.className = "todo-text"; // Add a class for text styling
 
-            // Create a delete button
+            const completedButton = document.createElement("button");
+            completedButton.className = "completed-btn";
+            completedButton.addEventListener("click", (e) => {
+                const todoItem = e.target.parentElement;
+                const todoTextElement = todoItem.querySelector(".todo-text");
+
+                if (todoTextElement) {
+                    todoTextElement.classList.toggle("completed");
+                    completedButton.classList.toggle("checkmark");
+                }
+            });
+
             const deleteButton = document.createElement("button");
             deleteButton.className = "delete-btn";
             deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
@@ -23,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 todoItem.remove();
             });
 
-            // Append elements to the todo item
+            todoItem.appendChild(completedButton);
             todoItem.appendChild(todoTextElement);
             todoItem.appendChild(deleteButton);
 
@@ -32,29 +41,15 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    // Handle form submission to add a new todo
     document.querySelector(".formselect").addEventListener("submit", function (e) {
         e.preventDefault();
         addTodo();
     });
 
-    // Handle pressing Enter to add a new todo
     todoInput.addEventListener("keydown", function (e) {
         if (e.key === "Enter") {
             e.preventDefault();
             addTodo();
-        }
-    });
-
-    // Handle clicking on the todo item to toggle the "done" status
-    todoList.addEventListener("click", function (e) {
-        if (e.target.classList.contains("todo-text")) {
-            const todoTextElement = e.target;
-            if (!todoTextElement.classList.contains("completed")) {
-                todoTextElement.classList.add("completed");
-            } else {
-                todoTextElement.classList.remove("completed");
-            }
         }
     });
 });
